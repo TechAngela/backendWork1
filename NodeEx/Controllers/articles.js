9ee009a055cd415c245b9ac93c38d6e25658b039
Angelique Uwimbabazi <uwangela12@gmail.com>
2023-05-25 02:12:26 +0200

    crud operations on articles

--- NodeEx/Controllers/articles.js
+++ NodeEx/Controllers/articles.js
@@ -26,48 +26,128 @@ const createArticle = async (req, res) => {
           res.send(error)
      }
 }
-const readArticle = (req, res) => {
-
-     try {
-           const blogid = req.params.reqid;
-           
-          
-     } catch (error) {
-          console.log("error occured" + error)
-          res.status(500).json({
-               error: error
-
-          })
+const readArticle =async (req, res)=>{
+     try{
+         const blogId = req.params.reqId;
+         const query = {_id:blogId};
+         
+         const result = await ArticleModel.find(query);  
+         //ArticleModel.find({_id:blogId})
+         if(result.length == 0){
+             res.status(404).json({
+                 message:"Data fetched but nothing was found",
+                 error:"No data found",
+                 data:result
+             })
+         }else{
+             res.status(200).json({
+                 message:"Data fetched successfully",
+                 error:null,
+                 data:result
+             });
+         }
+         range
+ 
+         // res.status(name);
+ 
      }
-}
+     catch(err){
+         console.log("Error under readArticleOne")
+         res.status(500).json({
+             message:"Failed to read the article specified",
+             error:"Internal error server",
+             data:null
+         })
+     }
+    
+ 
+ }
+
+ const readArticlesLimited = async(req, res) =>{
+     try{
+         const page = req.query.page || 1;
+         const range = req.query.limit || 5;
+     
+         let limitation = range * page;    // 10 * 2 = 20
+         let start = limitation - range;  // 20 - 10  = 10 
+     
+         let result = await ArticleModel.find({}).skip(start).limit(range)
+         // here handle the situation when the resuls are empty
+         res.status(200).json({
+             message:"Data fetched successfully",
+             error:null,
+             data: result
+         });
+     }
+     catch(err) {
+         console.log("Error under readARticlesLimited")
+         res.status(500).json({
+             message:"Failed to read the articles specified",
+             error:"Internal error server",
+             data:null
+         })
+     }
+ 
+ 
+ 
+ }
+ 
 const readMoreArticles = (req, res) => {
      const start = req.query.start;
      // res.send(start);
      let a = req.query.name
      res.send(req.query);//to get both
 }
-// const putExerciseArticles =(req , res)=>{
-//      let queryId = req.query.id;
-//      let bodyData = req.body;
-//      res.send(queryId);
-// }
-
-const deleteArticle = (req, res) => {
-     // res.send("this is the delete article controller")
-     let a = req.query.id;
-     res.send(req.query);
-
+const putExerciseArticles =(req , res)=>{
+     let queryId = req.query.id;
+     let bodyData = req.body;
+     res.send([queryId , bodyData]);
 }
-const updateArticle = (req, res) => {
-     const id = req.params.id;
-     const bodyData = req.body;
-     try {
-          res.send([id, bodyData]);
-     } catch (error) {
-          console.log("error occured" + error)
+
+const deleteArticle = async (req, res)=>{
+     const articleId = req.params.articleId;
+     let found = await ArticleModel.find({_id:articleId});
+     if(found.length == 0){
+         res.status(404).json({
+             message:"Article trying to delete is not available",
+             error:"Article not found",
+             data:null
+         });
+     }else{
+ 
+         let result = await ArticleModel.deleteOne({_id:articleId});
+ 
+         res.status(200).json({
+             message:"Article"+ articleId +" deleted successfully",
+             error:null,
+             data:result
+         });
      }
-}
+ }
+const updateArticle =async (req,res)=>{
+     const articleId = req.params.articleId;
+     const updates = req.body;
+     let found = await ArticleModel.find({_id:articleId});
+     if(found.length ==0){
+         res.status(404).json({
+             message:"Article trying to update is not available",
+             error:"Article not found",
+             data:null
+         });
+     }else{
+         let result = await ArticleModel.findOneAndUpdate(
+             {_id:articleId},
+             {$set:updates}
+         )
+         res.status(200).json({
+             message:"Data updated successfully",
+             error:null,
+             data: result
+         })
+     }
+ 
+ }
 
 
 
-export { createArticle, readArticle, deleteArticle, readMoreArticles, updateArticle}
+export { createArticle, readArticle, deleteArticle, readMoreArticles, updateArticle , readArticlesLimited , putExerciseArticles}
--- NodeEx/Routes/articles.js
+++ NodeEx/Routes/articles.js
@@ -1,15 +1,17 @@
 import express from "express"
 import bodyParser from "body-parser";
-import { createArticle , readArticle , deleteArticle , readMoreArticles , updateArticle } from "../Controllers/articles.js";
+import { createArticle,readArticle,readMoreArticles, putExerciseArticles,readArticlesLimited,updateArticle, deleteArticle} from "../Controllers/articles.js";
 
 const router = express.Router();
 router.use(bodyParser.json());
 
-router.post ("/", createArticle);
+router.post ("/create", createArticle);
 router.post ("/names/:yourname", readArticle);
 router.get("/more", readMoreArticles)
 router.put ("/names/:id" , updateArticle);
 router.delete("/names/:id" ,deleteArticle )
+router.put("/exercise",putExerciseArticles);
+router.get("/", readArticlesLimited);
 
 
 
